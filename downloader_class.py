29ddb29c5b2cd8d50376bd5404040c59085aced7
Ilyua <old.flanigan@gmail.com>
2017-06-02 19:20:40 +0300

    class 0.0.2

--- downloader_class.py
+++ downloader_class.py
@@ -1,7 +1,7 @@
 import re
 import sys
-import pprint
-import apiclient
+from pprint import pprint
+
 from apiclient.discovery import build
 from apiclient.errors import HttpError
 from oauth2client.tools import argparser
@@ -16,36 +16,17 @@ class Downloader:
                     YOUTUBE_API_VERSION,
                     developerKey=DEVELOPER_KEY)
 
-    def __init__(number_of page):
+    def __init__(self, number_of_page):
         self.number_of_page = number_of_page
         self.videos_info = []
         self.channels_array = []
         self.channels__of_comments_array = []
 
-    def youtube_search(key_word):
-        page_token = None
-        search_videos = []
-        for number in range(self.number_of_pages):
-            search_response = youtube.search().list(
-                q=key_word,
-                type="video",
-                part="id,snippet",
-                maxResults=1,
-                pageToken=page_token
-            ).execute()
-            page_token = search_response.get("nextPageToken", None)
-            if page_token == None:
-                break
-            for search_result in search_response.get("items", []):
-                search_videos.append(search_result["id"]["videoId"])
-        videos_info = []
-        for video_id in search_videos:
-            video_response = youtube.videos().list(
-                id=video_id,
-                part='snippet, recordingDetails'
-            ).execute()
-        videos_info.extend(video_response.get("items", []))
-        return videos_info
+    def search_by_all_key():
+        words_list = search_dictionary_parser('5000lemma.txt')
+        word = words_list[0]
+        youtube_search(word)
+        pprint(self.videos_info)
 
     def create_channels_list(videos_array):
         channels_ids = []
@@ -87,23 +68,47 @@ class Downloader:
             ).execute()
             channels__of_comments_array.extend(result.get("items", []))
         return channels__of_comments_array
+#####################################################################
 
-    private:
+    def youtube_search(key_word):
+        page_token = None
+        search_videos = []
+        for number in range(self.number_of_pages):
+            search_response = youtube.search().list(
+                q=key_word,
+                type="video",
+                part="id,snippet",
+                maxResults=1,
+                pageToken=page_token
+            ).execute()
+            page_token = search_response.get("nextPageToken", None)
+            if page_token == None:
+                break
+            for search_result in search_response.get("items", []):
+                search_videos.append(search_result["id"]["videoId"])
+        videos_info = []
+        for video_id in search_videos:
+            video_response = youtube.videos().list(
+                id=video_id,
+                part='snippet, recordingDetails'
+            ).execute()
+        videos_info.extend(video_response.get("items", []))
+        return videos_info
 
     def search_dictionary_parser(words_file_name):
-    with open(words_file_name) as my_file:
-        file_string = my_file.read()
-    return re.findall(r'[а-я]+', file_string)
+        with open(words_file_name) as my_file:
+            file_string = my_file.read()
+        return re.findall(r'[а-я]+', file_string)
 
     def uniquer(seq, f=None):
-    """ Keeps earliest occurring item of each f-defined equivalence class """
-    if f is None:    # f's default is the identity function f(x) -> x
-        def f(x): return x
-    already_seen = set()
-    result = []
-    for item in seq:
-        marker = f(item)
-        if marker not in already_seen:
-            already_seen.add(marker)
-            result.append(item)
-    return result
+        """ Keeps earliest occurring item of each f-defined equivalence class """
+        if f is None:    # f's default is the identity function f(x) -> x
+            def f(x): return x
+        already_seen = set()
+        result = []
+        for item in seq:
+            marker = f(item)
+            if marker not in already_seen:
+                already_seen.add(marker)
+                result.append(item)
+        return result
---
+++ main.py
@@ -0,0 +1,5 @@
+import downloader_class
+
+downloader = Downloader(2)
+
+downloader.search_by_all_key
